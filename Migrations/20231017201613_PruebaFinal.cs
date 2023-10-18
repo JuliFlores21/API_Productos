21db98c5ba24e1ebb0f4d596d7f0d32a13144567
unknown <julian.flores.armijos@udla.edu.ec>
2023-10-18 11:18:21 +0200

    API Final

---
+++ Migrations/20231017201613_PruebaFinal.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API_Productos.Migrations
+{
+    /// <inheritdoc />
+    public partial class PruebaFinal : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
